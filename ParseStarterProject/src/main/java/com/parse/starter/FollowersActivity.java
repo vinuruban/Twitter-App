b226a0e08e28199903332f6e577ba683a4314084
Vinukshen <vinukshen@gmail.com>
2020-08-19 13:58:33 +0100

    checkbox replaced with colours to indicate if user is isFollowing or not + these data gets updated in Parse under isFollowing column

--- ParseStarterProject/src/main/java/com/parse/starter/FollowersActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/FollowersActivity.java
@@ -1,7 +1,17 @@
 package com.parse.starter;
 
+import android.annotation.TargetApi;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,11 +37,36 @@ public class FollowersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_followers);
 
+        setTitle("Followers");
+
         users = new ArrayList<UserObject>();
         adapter = new UserAdapter(FollowersActivity.this, users);
         listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
 
-        setTitle("Followers");
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //To use colours to indicate if isFollowing or not
+                ColorDrawable viewColor = (ColorDrawable) view.getBackground();
+
+                if (viewColor.getColor() == Color.WHITE) {
+                    Toast.makeText(getApplicationContext(), "Checked " + users.get(i).getUsername(), Toast.LENGTH_SHORT).show();
+                    ParseUser.getCurrentUser().add("isFollowing", users.get(i).getUsername()); //add user to the list of followers
+                    view.setBackgroundColor(Color.CYAN);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Unchecked " + users.get(i).getUsername(), Toast.LENGTH_SHORT).show();
+                    ParseUser.getCurrentUser().getList("isFollowing").remove(users.get(i).getUsername()); //remove user
+                    List newList = ParseUser.getCurrentUser().getList("isFollowing"); //creates a updated list of users
+                    ParseUser.getCurrentUser().remove("isFollowing");
+                    ParseUser.getCurrentUser().put("isFollowing", newList);
+                    view.setBackgroundColor(Color.WHITE);
+                }
+                ParseUser.getCurrentUser().saveInBackground(); //actually updates the list in Parse
+            }
+        });
 
         //Get current username
         currentUser = ParseUser.getCurrentUser().getUsername();
@@ -49,7 +84,7 @@ public class FollowersActivity extends AppCompatActivity {
                         String username = user.getUsername();
                         users.add(new UserObject(username));
                     }
-                    listView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
                 }
                 else {
                     e.printStackTrace();
